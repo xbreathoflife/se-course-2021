03e9fbd9fe4da7e158d6318a00f3a2fff45ef66c
German Tarabonda <tarabonda@bk.ru>
2021-11-09 23:29:06 +0300

    Add weights to edges

--- src/main/kotlin/ru/hse/smartUniversity/UniversityFlowMap.kt
+++ src/main/kotlin/ru/hse/smartUniversity/UniversityFlowMap.kt
@@ -4,22 +4,32 @@ class UniversityFlowMap {
     private data class Room(val name: String) {
         val neighbors = mutableSetOf<Room>()
         val capacity = mutableMapOf<Room, Long>()
+        val timings = mutableMapOf<Room, Long>()
     }
 
+    data class Passage(val from: String?, val to: String?)
+
     private val rooms = mutableMapOf<String, Room>()
-    private fun connect(first: Room?, second: Room?, capacity: Long) {
-        if (second != null) first?.neighbors?.add(second)
-        if (second != null) first?.capacity?.put(second, capacity)
-        if (first != null) second?.neighbors?.add(first)
-        if (first != null) second?.capacity?.put(first, capacity)
+    private val edges = mutableListOf<Passage>()
+
+    private fun addForRoom(room: Room?, sndRoom: Room?, capacity: Long, time: Long) {
+        if (sndRoom != null) room?.neighbors?.add(sndRoom)
+        if (sndRoom != null) room?.capacity?.put(sndRoom, capacity)
+        if (sndRoom != null) room?.timings?.put(sndRoom, time)
+        edges.add(Passage(room?.name, sndRoom?.name))
+    }
+
+    private fun connect(first: Room?, second: Room?, capacity: Long, time: Long) {
+        addForRoom(first, second, capacity, time)
+        addForRoom(second, first, capacity, time)
     }
 
     fun addRoom(name: String) {
         rooms[name] = Room(name)
     }
 
-    fun connect(first: String, second: String, dist: Long = 1) =
-        connect(rooms[first], rooms[second], dist)
+    fun connect(first: String, second: String, capacity: Long = 1, time: Long = 1) =
+        connect(rooms[first], rooms[second], capacity, time)
 
     fun neighbors(name: String): List<String> =
         rooms[name]?.neighbors?.map { it.name } ?: listOf()
@@ -31,6 +41,12 @@ class UniversityFlowMap {
         val curDist = getCapacity(from, to)
         rooms[from]?.capacity?.replace(rooms.getValue(to), curDist + dist)
     }
+
+    fun getTime(from: String, to: String): Long =
+        rooms[from]?.timings?.get(rooms[to]) ?: 1
+
+    fun getAllPassages(): List<Passage> =
+        edges
 
     fun getAllRooms(): List<String> =
         rooms.keys.toList()
--- src/test/kotlin/ru/hse/smartUniversity/FordFulkersonTest.kt
+++ src/test/kotlin/ru/hse/smartUniversity/FordFulkersonTest.kt
@@ -24,4 +24,24 @@ internal class FordFulkersonTest {
         val m = FordFulkerson()
         assertEquals(6, m.fordFulkerson(map, "101", "106"))
     }
+
+    @Test
+    fun checkMaxMincost() {
+        val map = UniversityFlowMap()
+        map.addRoom("101")
+        map.addRoom("102")
+        map.addRoom("103")
+        map.addRoom("104")
+        map.addRoom("105")
+        map.addRoom("106")
+        map.connect("101", "102", 2, 2)
+        map.connect("102", "103", 2, 1)
+        map.connect("102", "104", 2, 10)
+        map.connect("103", "105", 2, 2)
+        map.connect("104", "105", 2, 20)
+        map.connect("105", "106", 2, 1)
+
+        val m = MincostFlow()
+        assertEquals(12, m.findMinCost(map, "101", "106"))
+    }
 }
